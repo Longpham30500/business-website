9c96160f2846f90008994971a446a47317762c66
Phạm Long <phamlong@MacBook-Pro-cua-Long.local>
2023-10-30 21:20:12 +0700

    add slick-carousel library and effect

--- js/main.js
+++ js/main.js
@@ -1,46 +1,45 @@
-// Xử lý khi người dùng bấm ra ngoài header menu
-document.addEventListener('click', function(e) {
-    let headerMenu = document.querySelector('.header__menu')
-    if (!headerMenu.contains(e.target)) {
-        // Xử lý ẩn mobile menu khi người dùng bấm ra ngoài
-        document.querySelector('.header__menu-list').classList.remove('mobile__menu'); 
-        
-        // Xử lý ẩn sub-menu khi người dùng bấm ra ngoài
-        document.querySelectorAll('.header__drop-down').forEach((headerDropDown) => {
-            headerDropDown.querySelector('.drop-down__list').classList.remove('active');
-        })
-    }
-});
+$(function(){
 
-// Xử lý khi người dùng bấm vào mobile-menu-button
-document.querySelector('.header__menu-btn').addEventListener('click', function() {
-    // Ẩn sub-menu
-    document.querySelectorAll('.header__drop-down').forEach((headerDropDown) => {
-        let dropdownList = headerDropDown.querySelector('.drop-down__list');
-        if (dropdownList.classList.contains('active')) {
-            dropdownList.classList.remove('active');
-        }
+    $('.slider__inner').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        autoplaySpeed: 2000,
+        autoplay: true,
     });
-    // Ẩn/hiện mobile menu
-    let dropdown = document.querySelector('.header__menu-list');
-    if (dropdown.classList.contains('mobile__menu')) {
-        dropdown.classList.remove('mobile__menu');
-    } else {
-        dropdown.classList.add('mobile__menu');
-    }
-});
 
-// Xử lý ẩn/hiện sub-menu
-document.querySelectorAll('.header__drop-down').forEach((headerDropDown) => {
-    headerDropDown.addEventListener('click', function() {
-        // Lấy phần tử .drop-down__list trong phạm vi của phần tử headerDropDown được bấm
-        let dropdownList = headerDropDown.querySelector('.drop-down__list');
-        // Kiểm tra xem phần tử .drop-down__list này đã có class "active" hay chưa
-        if (dropdownList.classList.contains('active')) {
-            dropdownList.classList.remove('active');
-        } else {
-            dropdownList.classList.add('active');
-        }
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'  <button class="slick-arrow slick-prev"><img src="../images/icons/bluearrow_left.png" alt=""></button>',
+        nextArrow:'  <button class="slick-arrow slick-next"><img src="../images/icons/bluearrow_right.png" alt=""></button>', 
+        responsive:[
+        {
+            breakpoint: 911,
+            settings:{
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 681,
+            settings:{
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 455,
+            settings:{
+                slidesToShow: 1,
+            }
+        }]      
     });
-});
 
+    $('.header__menu-btn').on('click', function(){
+        $('.header__menu > ul').slideToggle();
+    });
+    $('.header__drop-down.drop-down').on('click', function(){
+        $(this).children('.drop-down__list').toggleClass('active');
+        $(this).children('.drop-down__link').toggleClass('active');
+    });
+
+});
